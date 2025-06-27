8bbfeede32a26760b580159b27c8ada522a3e08b
Neha Garg <gargneha2409@gmail.com>
2025-06-27 15:50:48 +0530

    server fixed

--- api/server.js
+++ api/server.js
@@ -8,7 +8,7 @@ console.log('🔑 ENV GMAIL_PASS:', process.env.GMAIL_PASS ? '****' : 'MISSING')
 
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors()); // Enable CORS for frontend requests
@@ -72,7 +72,3 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-app.get('/api/ping', (req, res) => {
-  console.log('👉 Received GET /api/ping');
-  res.send('pong');
-});
